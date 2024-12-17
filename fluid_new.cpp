1130dc7a73a46539f9bece88da2c296125f4a79a
mregrock <78217843+mregrock@users.noreply.github.com>
2024-12-17 20:43:40 +0300

    Begin second step

--- fluid_new.cpp
+++ fluid_new.cpp
@@ -10,11 +10,9 @@
 
 using namespace std;
 
-constexpr size_t N = 36, M = 84;
-constexpr size_t T = 1'000'000;
-constexpr std::array<pair<int, int>, 4> deltas{{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}};
-
-char field[N][M + 1] = {
+// Константы и начальное поле
+constexpr size_t DEFAULT_N = 36, DEFAULT_M = 84;
+constexpr char initial_field[DEFAULT_N][DEFAULT_M + 1] = {
     "####################################################################################",
     "#                                                                                  #",
     "#                                                                                  #",
@@ -53,6 +51,7 @@ char field[N][M + 1] = {
     "####################################################################################",
 };
 
+// Оставляем Fixed как есть
 struct Fixed {
     constexpr Fixed(int v): v(v << 16) {}
     constexpr Fixed(float f): v(f * (1 << 16)) {}
@@ -71,79 +70,62 @@ struct Fixed {
     bool operator==(const Fixed&) const = default;
 };
 
-Fixed operator+(Fixed a, Fixed b) {
-    return Fixed::from_raw(a.v + b.v);
-}
-
-Fixed operator-(Fixed a, Fixed b) {
-    return Fixed::from_raw(a.v - b.v);
-}
-
-Fixed operator*(Fixed a, Fixed b) {
-    return Fixed::from_raw(((int64_t) a.v * b.v) >> 16);
-}
-
-Fixed operator/(Fixed a, Fixed b) {
-    return Fixed::from_raw(((int64_t) a.v << 16) / b.v);
-}
-
-Fixed &operator+=(Fixed &a, Fixed b) {
-    return a = a + b;
-}
-
-Fixed &operator-=(Fixed &a, Fixed b) {
-    return a = a - b;
-}
-
-Fixed &operator*=(Fixed &a, Fixed b) {
-    return a = a * b;
-}
-
-Fixed &operator/=(Fixed &a, Fixed b) {
-    return a = a / b;
-}
-
-Fixed operator-(Fixed x) {
-    return Fixed::from_raw(-x.v);
-}
-
+// Операторы для Fixed
+Fixed operator+(Fixed a, Fixed b) { return Fixed::from_raw(a.v + b.v); }
+Fixed operator-(Fixed a, Fixed b) { return Fixed::from_raw(a.v - b.v); }
+Fixed operator*(Fixed a, Fixed b) { return Fixed::from_raw(((int64_t) a.v * b.v) >> 16); }
+Fixed operator/(Fixed a, Fixed b) { return Fixed::from_raw(((int64_t) a.v << 16) / b.v); }
+Fixed &operator+=(Fixed &a, Fixed b) { return a = a + b; }
+Fixed &operator-=(Fixed &a, Fixed b) { return a = a - b; }
+Fixed &operator*=(Fixed &a, Fixed b) { return a = a * b; }
+Fixed &operator/=(Fixed &a, Fixed b) { return a = a / b; }
+Fixed operator-(Fixed x) { return Fixed::from_raw(-x.v); }
 Fixed abs(Fixed x) {
     if (x.v < 0) {
         x.v = -x.v;
     }
     return x;
 }
+ostream &operator<<(ostream &out, Fixed x) { return out << x.v / (double) (1 << 16); }
 
-ostream &operator<<(ostream &out, Fixed x) {
-    return out << x.v / (double) (1 << 16);
-}
-
-struct VectorField {
-    array<Fixed, deltas.size()> v[N][M];
-    Fixed &add(int x, int y, int dx, int dy, Fixed dv) {
-        return get(x, y, dx, dy) += dv;
-    }
-
-    Fixed &get(int x, int y, int dx, int dy) {
-        size_t i = ranges::find(deltas, pair(dx, dy)) - deltas.begin();
-        assert(i < deltas.size());
-        return v[x][y][i];
-    }
-};
-
-Fixed rho[256];
-Fixed p[N][M]{}, old_p[N][M];
-VectorField velocity{}, velocity_flow{};
-int last_use[N][M]{};
-int UT = 0;
-mt19937 rnd(1337);
-int dirs[N][M]{};
-
+// Шаблонный класс FluidSimulator
+template<
+    typename NumericType = Fixed,  // тип для вычислений (Fixed по умолчанию)
+    size_t SizeN = DEFAULT_N,    // высота поля
+    size_t SizeM = DEFAULT_M     // ширина поля
+>
 class FluidSimulator {
+public:
+    struct VectorField {
+        array<NumericType, 4> v[SizeN][SizeM];
+        NumericType &add(int x, int y, int dx, int dy, NumericType dv) {
+            return get(x, y, dx, dy) += dv;
+        }
+
+        NumericType &get(int x, int y, int dx, int dy) {
+            size_t i = ranges::find(deltas, pair(dx, dy)) - deltas.begin();
+            assert(i < deltas.size());
+            return v[x][y][i];
+        }
+    };
+
 private:
-    Fixed (&rho_ref)[256];
-    Fixed (&p_ref)[N][M];
-    Fixed (&old_p_ref)[N][M];
+    static constexpr size_t N = SizeN;
+    static constexpr size_t M = SizeM;
+    static constexpr size_t T = 1'000'000;
+    static constexpr std::array<pair<int, int>, 4> deltas{{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}};
+
+    NumericType rho[256];
+    NumericType p[N][M]{}, old_p[N][M];
+    VectorField velocity{}, velocity_flow{};
+    int last_use[N][M]{};
+    int UT{0};
+    mt19937 rnd{1337};
+    int dirs[N][M]{};
+
+    NumericType (&rho_ref)[256];
+    NumericType (&p_ref)[N][M];
+    NumericType (&old_p_ref)[N][M];
     VectorField& velocity_ref;
     VectorField& velocity_flow_ref;
     int (&last_use_ref)[N][M];
@@ -235,10 +217,10 @@ private:
         Fixed cur_p;
         array<Fixed, deltas.size()> v;
         char (&field)[N][M + 1];
-        Fixed (&p)[N][M];
+        NumericType (&p)[N][M];
         VectorField& velocity;
 
-        ParticleParams(char (&f)[N][M + 1], Fixed (&p_arr)[N][M], VectorField& vel)
+        ParticleParams(char (&f)[N][M + 1], NumericType (&p_arr)[N][M], VectorField& vel)
             : field(f), p(p_arr), velocity(vel) {}
 
         void swap_with(int x, int y) {
@@ -304,9 +286,9 @@ private:
 
 public:
     FluidSimulator(
-        Fixed (&rho_arr)[256],
-        Fixed (&p_arr)[N][M],
-        Fixed (&old_p_arr)[N][M],
+        NumericType (&rho_arr)[256],
+        NumericType (&p_arr)[N][M],
+        NumericType (&old_p_arr)[N][M],
         VectorField& velocity,
         VectorField& velocity_flow,
         int (&last_use_arr)[N][M],
@@ -363,63 +345,55 @@ public:
         return propagate_flow(x, y, lim);
     }
 
-    friend int main();
-};
-
-int main() {
-    FluidSimulator simulator(rho, p, old_p, velocity, velocity_flow, last_use, UT, rnd, dirs, field);
-    
-    Fixed g = 0.1;
-
-    for (size_t i = 0; i < T; ++i) {
-        
+    void simulate_step(Fixed g, size_t iteration) {
         Fixed total_delta_p = 0;
+        
         // Apply external forces
         for (size_t x = 0; x < N; ++x) {
             for (size_t y = 0; y < M; ++y) {
-                if (field[x][y] == '#')
+                if (field_ref[x][y] == '#')
                     continue;
-                if (field[x + 1][y] != '#')
-                    velocity.add(x, y, 1, 0, g);
+                if (field_ref[x + 1][y] != '#')
+                    velocity_ref.add(x, y, 1, 0, g);
             }
         }
 
         // Apply forces from p
-        memcpy(old_p, p, sizeof(p));
+        memcpy(old_p_ref, p_ref, sizeof(p_ref));
         for (size_t x = 0; x < N; ++x) {
             for (size_t y = 0; y < M; ++y) {
-                if (field[x][y] == '#')
+                if (field_ref[x][y] == '#')
                     continue;
                 for (auto [dx, dy] : deltas) {
                     int nx = x + dx, ny = y + dy;
-                    if (field[nx][ny] != '#' && old_p[nx][ny] < old_p[x][y]) {
-                        auto delta_p = old_p[x][y] - old_p[nx][ny];
+                    if (field_ref[nx][ny] != '#' && old_p_ref[nx][ny] < old_p_ref[x][y]) {
+                        auto delta_p = old_p_ref[x][y] - old_p_ref[nx][ny];
                         auto force = delta_p;
-                        auto &contr = velocity.get(nx, ny, -dx, -dy);
-                        if (contr * rho[(int) field[nx][ny]] >= force) {
-                            contr -= force / rho[(int) field[nx][ny]];
+                        auto &contr = velocity_ref.get(nx, ny, -dx, -dy);
+                        if (contr * rho_ref[(int) field_ref[nx][ny]] >= force) {
+                            contr -= force / rho_ref[(int) field_ref[nx][ny]];
                             continue;
                         }
-                        force -= contr * rho[(int) field[nx][ny]];
+                        force -= contr * rho_ref[(int) field_ref[nx][ny]];
                         contr = 0;
-                        velocity.add(x, y, dx, dy, force / rho[(int) field[x][y]]);
-                        p[x][y] -= force / dirs[x][y];
-                        total_delta_p -= force / dirs[x][y];
+                        velocity_ref.add(x, y, dx, dy, force / rho_ref[(int) field_ref[x][y]]);
+                        p_ref[x][y] -= force / dirs_ref[x][y];
+                        total_delta_p -= force / dirs_ref[x][y];
                     }
                 }
             }
         }
 
         // Make flow from velocities
-        velocity_flow = {};
+        velocity_flow_ref = {};
         bool prop = false;
         do {
-            UT += 2;
+            UT_ref += 2;
             prop = 0;
             for (size_t x = 0; x < N; ++x) {
                 for (size_t y = 0; y < M; ++y) {
-                    if (field[x][y] != '#' && last_use[x][y] != UT) {
-                        auto [t, local_prop, _] = simulator.do_propagate_flow(x, y, 1);
+                    if (field_ref[x][y] != '#' && last_use_ref[x][y] != UT_ref) {
+                        auto [t, local_prop, _] = propagate_flow(x, y, 1);
                         if (t > 0) {
                             prop = 1;
                         }
@@ -431,49 +405,79 @@ int main() {
         // Recalculate p with kinetic energy
         for (size_t x = 0; x < N; ++x) {
             for (size_t y = 0; y < M; ++y) {
-                if (field[x][y] == '#')
+                if (field_ref[x][y] == '#')
                     continue;
                 for (auto [dx, dy] : deltas) {
-                    auto old_v = velocity.get(x, y, dx, dy);
-                    auto new_v = velocity_flow.get(x, y, dx, dy);
+                    auto old_v = velocity_ref.get(x, y, dx, dy);
+                    auto new_v = velocity_flow_ref.get(x, y, dx, dy);
                     if (old_v > 0) {
                         assert(new_v <= old_v);
-                        velocity.get(x, y, dx, dy) = new_v;
-                        auto force = (old_v - new_v) * rho[(int) field[x][y]];
-                        if (field[x][y] == '.')
-                            force *= 0.8;
-                        if (field[x + dx][y + dy] == '#') {
-                            p[x][y] += force / dirs[x][y];
-                            total_delta_p += force / dirs[x][y];
+                        velocity_ref.get(x, y, dx, dy) = new_v;
+                        auto force = (old_v - new_v) * rho_ref[(int) field_ref[x][y]];
+                        if (field_ref[x][y] == '.')
+                            force *= Fixed(0.8);
+                        if (field_ref[x + dx][y + dy] == '#') {
+                            p_ref[x][y] += force / dirs_ref[x][y];
+                            total_delta_p += force / dirs_ref[x][y];
                         } else {
-                            p[x + dx][y + dy] += force / dirs[x + dx][y + dy];
-                            total_delta_p += force / dirs[x + dx][y + dy];
+                            p_ref[x + dx][y + dy] += force / dirs_ref[x + dx][y + dy];
+                            total_delta_p += force / dirs_ref[x + dx][y + dy];
                         }
                     }
                 }
             }
         }
 
-        UT += 2;
+        UT_ref += 2;
         prop = false;
         for (size_t x = 0; x < N; ++x) {
             for (size_t y = 0; y < M; ++y) {
-                if (field[x][y] != '#' && last_use[x][y] != UT) {
-                    if (simulator.get_random01() < simulator.get_move_prob(x, y)) {
+                if (field_ref[x][y] != '#' && last_use_ref[x][y] != UT_ref) {
+                    if (random01() < move_prob(x, y)) {
                         prop = true;
-                        simulator.do_propagate_move(x, y, true);
+                        propagate_move(x, y, true);
                     } else {
-                        simulator.do_propagate_stop(x, y, true);
+                        propagate_stop(x, y, true);
                     }
                 }
             }
         }
 
         if (prop) {
-            cout << "Tick " << i << ":\n";
+            cout << "Tick " << iteration << ":\n";
             for (size_t x = 0; x < N; ++x) {
-                cout << field[x] << "\n";
+                cout << field_ref[x] << "\n";
             }
         }
     }
+
+    void run(Fixed g = Fixed(0.1)) {
+        for (size_t i = 0; i < T; ++i) {
+            simulate_step(g, i);
+        }
+    }
+
+    friend int main();
+};
+
+// Глобальные переменные для совместимости со старым кодом
+Fixed rho[256];
+Fixed p[DEFAULT_N][DEFAULT_M]{}, old_p[DEFAULT_N][DEFAULT_M];
+char field[DEFAULT_N][DEFAULT_M + 1];
+int last_use[DEFAULT_N][DEFAULT_M]{};
+int UT = 0;
+mt19937 rnd(1337);
+int dirs[DEFAULT_N][DEFAULT_M]{};
+
+// Нужно также объявить глобальные VectorField
+typename FluidSimulator<>::VectorField velocity{}, velocity_flow{};
+
+int main() {
+    // Копируем начальное поле
+    memcpy(field, initial_field, sizeof(field));
+
+    // Создаем симулятор с параметрами по умолчанию
+    FluidSimulator<> simulator(rho, p, old_p, velocity, velocity_flow, last_use, UT, rnd, dirs, field);
+    simulator.run();
+    return 0;
 }
